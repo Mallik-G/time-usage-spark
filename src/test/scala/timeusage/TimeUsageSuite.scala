c1109d255948ded601755d246c414957f767eda3
Ruhong <seahrh@gmail.com>
2017-03-19 16:40:24 +0800

    Baseline

---
+++ src/test/scala/timeusage/TimeUsageSuite.scala
@@ -0,0 +1,17 @@
+package timeusage
+
+import org.apache.spark.sql.{ColumnName, DataFrame, Row}
+import org.apache.spark.sql.types.{
+  DoubleType,
+  StringType,
+  StructField,
+  StructType
+}
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{BeforeAndAfterAll, FunSuite}
+
+import scala.util.Random
+
+class TimeUsageSuite extends FunSuite with BeforeAndAfterAll {
+
+}
